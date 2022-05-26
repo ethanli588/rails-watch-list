825abadacd0b7ea6a7851a4b11b8f82a8ef64914
Ethan Li <ethanli588@gmail.com>
2022-05-26 16:47:01 +1000

    Prepare rails app with external specs

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,7 +17,11 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
 
+    redirect_to list_path(@list)
   end
 
   private
