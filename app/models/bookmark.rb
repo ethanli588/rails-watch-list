a8ea67728fd04aecbd85dad6ffb753786b685f61
Ethan Li <ethanli588@gmail.com>
2022-05-27 11:45:28 +1000

    fix a bug about the length of the comment in the Bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
 
-  validates :comment, length: { minimum: 5 }
+  validates :comment, length: { minimum: 6 }
   validates :list, uniqueness: { scope: :movie }
 end
